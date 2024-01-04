43fb722cc0b53bdf8ae985e2077be3928ff8050e
James Liu <jamesliu@jamess-mbp.mynetworksettings.com>
2024-01-04 18:23:00 -0500

    simplest scraping script

---
+++ simple_web_scraping.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+# very simple web scraping script 
+# directly from online tutorial: https://www.youtube.com/watch?v=QhD015WUMxE
+
+page_to_scrape = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
+quotes = soup.findAll("span", attrs={"class":"text"})
+authors = soup.findAll("small", attrs={"class":"author"})
+
+for quote in quotes:
+    print(quote.text)
+
+for author in authors:
+    print(author.text)
