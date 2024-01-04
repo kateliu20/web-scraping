37b40908f4dd928d18ce18cafaa5338f03778e0f
Kate Liu <kateliu20@gmail.com>
2024-01-04 18:53:43 -0500

    added csv component

--- simple_web_scraping.py
+++ simple_web_scraping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 # very simple web scraping script
 # directly from online tutorial: https://www.youtube.com/watch?v=QhD015WUMxE
@@ -10,5 +11,12 @@ soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 quotes = soup.findAll("span", attrs={"class":"text"})
 authors = soup.findAll("small", attrs={"class":"author"})
 
+file = open("scraped_quotes.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES ", "AUTHORS"])
+
 for quote, author in zip(quotes, authors):
     print(quote.text + " - " + author.text) 
+    writer.writerow([quote.text, author.text])
+file.close()
